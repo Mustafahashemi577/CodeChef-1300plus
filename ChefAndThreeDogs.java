9ff5f6e2b365e76a3fbf4ffbe822c7f3492b45ef
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-05-08 20:04:38 +0430

    Add files via upload
    ChefAndThreeDogs problem with difficulty of 1473 code in Java
    https://www.codechef.com/practice/course/2-star-difficulty-problems/DIFF1500/problems/CHDOGS

---
+++ ChefAndThreeDogs.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner scan = new Scanner (System.in);
+        int t = scan.nextInt();
+        while(t-->0){
+            double s = scan.nextDouble();
+            double v = scan.nextDouble();
+            System.out.println((2*s)/(3*v));
+        }
+	}
+}
+
